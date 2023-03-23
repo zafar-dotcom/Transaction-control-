308d8d086692ef64627791042247f6301f1f9a7e
Mohammad zafar <mohammadzafar12555@gmail.com>
2023-03-23 16:40:19 +0500

    Master Detail complete implementation with transaction control

--- Controllers/TransactionScopeController.cs
+++ Controllers/TransactionScopeController.cs
@@ -9,20 +9,58 @@ namespace SendEmailViaSMTP.Controllers
     [ApiController]
     public class TransactionScopeController : ControllerBase
     {
+        public DAL dal;
+        public TransactionScopeController()
+        {
+            dal = new DAL();
+
+        }
         [Route("transaction")]
         [HttpPost]
         public IActionResult Transaction([FromBody] TransactionModel modl)
         {
-            DAL obj = new DAL();
-            if (obj.TransactionScope(modl))
+            
+            if (dal.TransactionScope(modl))
             {
-              //  obj.BeginTransaction();
+              dal.BeginTransaction(modl);
                 return Ok();
 
             }
             
             else
             return BadRequest();
+        }
+        [Route("Get_Applicant_experience")]
+        [HttpGet]
+        public IActionResult ApplicantExperience()
+        {
+           var data= dal.GetfromMasterDEtail();
+            return Ok(data);
+        }
+        [Route("update_Applicant_experience")]
+        [HttpPut]
+        public IActionResult Update_master_detail(TransactionModel modl)
+        {
+            if (dal.Update_master_detail(modl))
+            {
+                return Ok();
+
+            }
+            else
+                return BadRequest();
+        }
+
+        [Route("delete_Applicant_experience")]
+        [HttpDelete]
+        public IActionResult Delete_master_detail(int id)
+        {
+            if (dal.Delete_master_Detail(id))
+            {
+                return Ok();
+
+            }
+            else
+                return BadRequest();
         }
     }
 }
--- DAL Services/DAL.cs
+++ DAL Services/DAL.cs
@@ -1,6 +1,8 @@
 ﻿using MySql.Data.MySqlClient;
 using SendEmailViaSMTP.Models;
 using System.Data;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
 using System.Transactions;
 
 namespace SendEmailViaSMTP.DAL_Services
@@ -14,7 +16,10 @@ namespace SendEmailViaSMTP.DAL_Services
         //}
 
         private readonly string str = "server=localhost;port=3306;uid=root;pwd=sobiazafar@2023;database=mvc_crud";
+       
         
+        // ---------------Transaction Control Start with master detail operation---------------------
+
         public bool TransactionScope(TransactionModel mdl)
         {
             using (var txscope = new TransactionScope(TransactionScopeOption.RequiresNew))
@@ -68,7 +73,7 @@ namespace SendEmailViaSMTP.DAL_Services
                 }
             }
         }
-        public bool BeginTransaction()
+        public bool BeginTransaction(TransactionModel mdl)
         {
            
             MySqlTransaction objTrans = null;
@@ -81,11 +86,42 @@ namespace SendEmailViaSMTP.DAL_Services
                 MySqlCommand objCmd2 = new MySqlCommand("insert into tblProjectMember(MemberID, ProjectID) values(9, 7)", objConn);
                 try
                 {
-                    objCmd1.ExecuteNonQuery();
-                    objCmd2.ExecuteNonQuery(); // Throws exception due to foreign key constraint   
-                    objTrans.Commit();
-                    return true;
-                }
+                    //objCmd1.ExecuteNonQuery();
+                    //objCmd2.ExecuteNonQuery(); // Throws exception due to foreign key constraint   
+                    //objTrans.Commit();
+                    //return true;
+                    using (MySqlConnection connection = new MySqlConnection(str))
+                    {
+                        connection.Open();
+                        // string query1 = "insert into Applicant(ProjectID,Name) values (LAST_INSERT_ID(),@name)";
+                        //string query2 = "insert into tblProjectMember(MemberID,ProjectID) values(@mid,@pid)";
+                        string query1 = "insert into Applicant(name,gender,age,qualificaion,total_experience) values (@name,@gender,@age,@qualificaion,@total_experience)";
+                        string query2 = "insert into experience(company_name,designation,years_worked,app_id) values(@company_name,@designation,@years_worked,@app_id)";
+
+                        MySqlCommand cmd1 = new MySqlCommand(query1, connection);
+                        // cmd1.Parameters.AddWithValue("@name", mdl.Name);
+                        cmd1.Parameters.AddWithValue("@name", mdl.Name);
+                        cmd1.Parameters.AddWithValue("@gender", mdl.Gender);
+                        cmd1.Parameters.AddWithValue("@age", mdl.Age);
+                        cmd1.Parameters.AddWithValue("@qualificaion", mdl.Qualificaion);
+                        cmd1.Parameters.AddWithValue("@total_experience", mdl.Total_Experience);
+                        cmd1.ExecuteNonQuery();
+                        long appid = cmd1.LastInsertedId;
+
+                        foreach (var experience in mdl.Experiences)
+                        {
+                            MySqlCommand cmd2 = new MySqlCommand(query2, objConn);
+                            cmd2.Parameters.AddWithValue("@company_name", experience.company_name);
+                            cmd2.Parameters.AddWithValue("@designation", experience.designation);
+                            cmd2.Parameters.AddWithValue("@years_worked", experience.years_worked);
+                            cmd2.Parameters.AddWithValue("@app_id", appid);
+                            cmd2.ExecuteNonQuery();
+
+                        }
+                        objTrans.Commit();
+                        return true;
+                    }
+                    }
                 catch (Exception)
                 {
                     objTrans.Rollback();
@@ -98,6 +134,164 @@ namespace SendEmailViaSMTP.DAL_Services
             }
 
         }
+
+        public List<TransactionModel> GetfromMasterDEtail()
+        {
+            using (MySqlConnection conn = new MySqlConnection(str))
+            {
+                DataTable tbl = new DataTable();
+                List<TransactionModel> lst = new List<TransactionModel>();
+               // TransactionModel obj = new TransactionModel();
+                conn.Open();
+                string query = "select a.app_id,e.app_id,a.name, a.age ,a.qualificaion,a.total_experience ,e.company_name,e.designation ,e.years_worked from \r\nApplicant a join experience e on a.app_id=e.app_id";
+                try
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandType = CommandType.Text;
+                        cmd.CommandText = query;
+                        MySqlDataReader dr = cmd.ExecuteReader();
+                        tbl.Load(dr);
+                        conn.Close();
+                        foreach (DataRow row in tbl.Rows)
+                        {
+                         TransactionModel obj=   new TransactionModel()
+                            {
+                                app_id = (int)row["app_id"],
+                                Name = row["name"].ToString(),
+                                Age = (int)row["age"],
+                                Qualificaion = row["qualificaion"].ToString(),
+                                Total_Experience = row["total_experience"].ToString(),
+                                Experiences = new List<Experience>()
+                            };
+
+                            Experience exp = new Experience()
+                            {
+                                app_id = (int)row["app_id"],
+                                company_name = row["company_name"].ToString(),
+                                designation = row["designation"].ToString(),
+                                years_worked = (int)row["years_worked"]
+                            };
+                            obj.Experiences.Add(exp);
+                            //check in the list if applicant already exit
+                            var exitingobj = lst.FirstOrDefault(x => x.app_id == obj.app_id);
+                            if (exitingobj != null)
+                            {
+                                exitingobj.Experiences.Add(exp);
+                            }
+                            else
+                            {
+                                lst.Add(obj);
+                            }
+
+
+                        }
+                    }
+                    return lst ;
+
+                }
+
+                catch (Exception)
+                {
+
+                    throw;
+                    
+                }
+            }
+        }
+
+        public bool Update_master_detail(TransactionModel modl)
+        {
+            using (var transscope = new TransactionScope(TransactionScopeOption.RequiresNew))
+            {
+
+
+                try
+                {
+                    using (MySqlConnection conn = new MySqlConnection(str))
+                    {
+                        conn.Open();
+                        string query1 = "update Applicant set name=@name,gender=@gender,age=@age,qualificaion=@qualificaion,total_experience=@total_experience where app_id=@app_id";
+                        MySqlCommand cmd1 = new MySqlCommand(query1, conn);
+                        cmd1.Parameters.AddWithValue("@name", modl.Name);
+                        cmd1.Parameters.AddWithValue("@gender", modl.Gender);
+                        cmd1.Parameters.AddWithValue("@age", modl.Age);
+                        cmd1.Parameters.AddWithValue("@qualificaion", modl.Qualificaion);
+                        cmd1.Parameters.AddWithValue("@total_experience", modl.Total_Experience);
+                        cmd1.Parameters.AddWithValue("@app_id", modl.app_id);
+                        cmd1.ExecuteNonQuery();
+
+                        cmd1.CommandText = "delete from experience where app_id=@app_id";
+                        cmd1.ExecuteNonQuery();
+
+                        foreach (var experience in modl.Experiences)
+                        {
+                            string query = "insert into experience (company_name,designation,years_worked,app_id)values(@company_name,@designation,@years_worked,@app_id)";
+                            MySqlCommand cmd2 = new MySqlCommand(query, conn);
+                            cmd2.Parameters.AddWithValue("@company_name", experience.company_name);
+                            cmd2.Parameters.AddWithValue("@designation", experience.designation);
+                            cmd2.Parameters.AddWithValue("@years_worked", experience.years_worked);
+                            cmd2.Parameters.AddWithValue("@app_id", modl.app_id);
+                            cmd2.ExecuteNonQuery();
+                            cmd2.Parameters.Clear();
+
+                        }
+
+                    }
+                    transscope.Complete();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    transscope.Dispose();
+                    throw;
+                    return false;
+                }
+            }
+        }
+
+
+        public bool Delete_master_Detail(int id)
+        {
+            using(var transsscope=new TransactionScope(TransactionScopeOption.RequiresNew))
+            {
+                try
+                {
+                    using (MySqlConnection conn = new MySqlConnection(str))
+                    {
+                        conn.Open();
+                        //first we should delete child record because if we delete parent then child refernce will be lost and it become free wastage
+                        string childquery = "delete from experience where app_id=@id";
+                        using (MySqlCommand cmd1 = new MySqlCommand(childquery, conn))
+                        {
+                            cmd1.Parameters.AddWithValue("@id", id);
+                            cmd1.ExecuteNonQuery();
+
+                        }
+                        //2nd step is to delete parent table            
+                        string parentquery = "delete from Applicant where app_id=@id";
+                        using (MySqlCommand cmd2 = new MySqlCommand(parentquery, conn))
+                        {
+                            cmd2.Parameters.AddWithValue("@id", id);
+                            cmd2.ExecuteNonQuery();
+
+                        }
+
+                    }
+                    transsscope.Complete();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    transsscope.Dispose();
+                    throw;
+                    return false;
+                }
+
+            }
+        }
+        // -----------------Transactopn Control End -----------------------------
         
         
         
